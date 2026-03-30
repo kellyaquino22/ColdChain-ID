ae53252b69ba85f75840bcba31ff0a26ecb144dd
kellyaquino22 <kellyaquino22@gmail.com>
2026-03-30 16:43:40 -0400

    config

---
+++ config/config_example.h
@@ -0,0 +1,11 @@
+#ifndef CONFIG_EXAMPLE_H
+#define CONFIG_EXAMPLE_H
+
+
+const char* ssid = "NOME_DO_SEU_WIFI";
+const char* password = "SENHA_DO_WIFI";
+const char* mqtt_server = "ENDERECO_DO_BROKER_HIVEMQ"; 
+const char* mqtt_user = "USUARIO_MQTT"; 
+const char* mqtt_pass = "SENHA_MQTT";
+
+#endif
